8f962c05fdbccd4f54610982a5f9d290edced2fd
Macho25 <marekpolednik7@gmail.com>
2024-08-04 20:54:27 +0200

    Database_klanu.py - Pridany nazvy klanu a list vsech klanu ale neni dodelane

---
+++ Database_klanu.py
@@ -0,0 +1,224 @@
+# Drakthar = 'Drakthar'
+# Volkarim = 'Volkarim'
+# Grimthor = 'Grimthor'
+# Zantril = 'Zantril'
+# Ravendark = 'Ravendark'
+# Falthorn = 'Falthorn'
+# Gorath = 'Gorath'
+# Vexarim = 'Vexarim'
+# Tarkhul = 'Tarkhul'
+# Brimvald = 'Brimvald'
+# Korveth = 'Korveth'
+# Myrkal = 'Myrkal'
+# Thragorn = 'Thragorn'
+# Xandor = 'Xandor'
+# Kharzul = 'Kharzul'
+# Zarath = 'Zarath'
+# Vorthak = 'Vorthak'
+# Draelith = 'Draelith'
+# Maraxus = 'Maraxus'
+# Nalthor = 'Nalthor'
+# Vorgrim = 'Vorgrim'
+# Zuldan = 'Zuldan'
+# Tyrnath = 'Tyrnath'
+# Kragmar = 'Kragmar'
+# Ygrath = 'Ygrath'
+
+
+
+
+
+
+
+
+
+clany = {
+    
+    "Drakthar": {
+        "od_vojaku": 3,      # generator od 3 do 10 clenu
+        "do_vojaku": 10,
+        "poskozeni_vojaka": 3
+    
+    },
+
+    "Volkarim": {
+        "od_vojaku": 3, 
+        "do_vojaku": 10,
+        "poskozeni_vojaka": 3
+
+
+    },
+    
+    "Grimthor": {
+
+
+
+    },
+    
+    "Zantril": {
+
+
+
+    },
+    
+    "Ravendark": {
+
+
+
+    },
+    
+    "Falthorn": {
+
+
+
+    },
+    
+    "Gorath": {
+
+
+
+    },
+    
+    "Vexarim": {
+
+
+
+    },
+    
+    "Tarkhul": {
+
+
+
+    },
+   
+    "Brimvald": {
+
+
+
+    },
+    
+    "Korveth": {
+
+
+
+    },
+    
+    "Myrkal": {
+
+
+
+    },
+    
+    "Thragorn": {
+
+
+
+    },
+   
+    "Xandor": {
+
+
+
+    },
+    
+    "Kharzul": {
+
+
+
+    },
+    
+    "Zarath": {
+
+
+
+    },
+   
+    "Vorthak": {
+
+
+
+    },
+   
+    "Draelith": {
+
+
+
+    },
+   
+    "Maraxus": {
+
+
+
+    },
+   
+    "Nalthor": {
+
+
+
+    },
+   
+    "Vorgrim": {
+
+
+
+    },
+   
+    "Zuldan": {
+
+
+
+    },
+   
+    "Tyrnath": {
+
+
+
+    },
+   
+    "Kragmar": {
+
+
+
+    },
+   
+    "Ygrath": {
+
+
+
+    },
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
